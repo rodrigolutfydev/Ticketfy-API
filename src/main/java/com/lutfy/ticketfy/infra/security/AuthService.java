6160bc24996b358b2d6cf9cf631150a0d50255e7
rodrigolutfydev <rodrigolutfy.dev@gmail.com>
2026-09-11 15:57:49 -0300

    feat: add AuthService implementing UserDetailsService

---
+++ src/main/java/com/lutfy/ticketfy/infra/security/AuthService.java
@@ -0,0 +1,23 @@
+package com.lutfy.ticketfy.infra.security;
+
+import com.lutfy.ticketfy.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
