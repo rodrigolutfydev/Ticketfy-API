c9362183306accdb285e0f4514809b542248ee80
rodrigolutfydev <rodrigolutfy.dev@gmail.com>
2026-09-11 15:52:55 -0300

    test: add generated application context test

---
+++ src/test/java/com/lutfy/ticketfy/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutfy.ticketfy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
