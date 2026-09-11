4879ac5be7731723caced1f600761e2dad832867
rodrigolutfydev <rodrigolutfy.dev@gmail.com>
2026-09-11 15:57:13 -0300

    feat: add UserController with public registration endpoint

---
+++ src/main/java/com/lutfy/ticketfy/user/UserController.java
@@ -0,0 +1,26 @@
+package com.lutfy.ticketfy.user;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDetailsDTO> register(@RequestBody @Valid UserRegistrationDTO data) {
+        var user = service.register(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new UserDetailsDTO(user));
+    }
+}
